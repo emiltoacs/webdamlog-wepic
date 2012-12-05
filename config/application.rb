803d9f98e7036a183ec2446cc911987d31ed8268
jules testard <jules.testard@lsv.ens-cachan.fr>
2012-12-05 10:47:20 +0100

    Removing netbeans project from build. Should have been removed before

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module WepimApp
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :image_file, :image_thumb_file, :image_small_file]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
