f5dd47300cd68da1871a96eb68fbc30d56122b64
jules testard <jules.testard@lsv.ens-cachan.fr>
2012-12-14 11:57:31 +0100

    About to start Facebook Wraper. Investigating Koala ruby gem.

--- test/unit/wl_launcher_test.rb
+++ test/unit/wl_launcher_test.rb
@@ -7,27 +7,5 @@ require 'lib/wl_launcher'
 class WLLauncherTest < Test::Unit::TestCase
   include WLLauncher
   
-  def setup
-    @port = 10000
-  end
-  
-  def teardown
-    
-  end
-  
-  def test_a_start_server
-    start_server('test',@port)
-  end
-  
-  def test_b_port_open
-    sleep(8)
-    assert_equal(true,port_open?('localhost',@port))
-    assert_equal(false,port_open?('localhost',@port+1))
-  end
-  
-  def test_c_destroy_server
-    #The method should destroy one process
-    sleep(10)
-    assert_equal(1,exit_server(@port))
-  end
+  #Please refer to acknowledgment_test under test/integration.
 end
