197d118c45f7dd5a68ea73fe96921c6709337131
jules testard <jules.testard@lsv.ens-cachan.fr>
2013-01-23 01:02:54 -0500

    Validation and security checked for demo.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ActiveRecord::Base
   attr_accessible :ip, :port, :username
   attr_accessible :active
+  validates :username, :format => { :with => /\A[a-zA-Z]+\z/,
+    :message => "Only letters allowed" }
 end
