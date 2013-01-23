07a5dce84314470317b1457b27b84e8f5e770865
Émilien <first.last@inria.fr>
2013-01-23 12:15:16 +0100

    Add bud instancitation support

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-
 # Initialize the rails application
 WepimApp::Application.initialize!
 
@@ -13,15 +12,48 @@ if WepimApp.is_manager?
 else
   require 'lib/wl_peer'
   require 'lib/webdamlog/wlbud'
+  require 'lib/wl_logger'  
+
+  # TODO WLE:START all this messy code for the peer should be moved into an appropriate structure
+
+  #Create a subclass of WL
+  klass = Class.new(WLBud::WL)
+  # TODO find a good name for the sub class, it should be uniq
+  self.class.class_eval "ClassWL#{ENV['USERNAME']}on#{ENV['PORT']} = klass"
+  peer_name = "peername_#{ENV['USERNAME']}on#{ENV['PORT']}"
+  # TODO find a good place to put the program file
+  program_file_dir = File.expand_path('../../tmp/rule_dir', __FILE__)
+  unless (File::directory?(program_file_dir))
+    Dir.mkdir(program_file_dir)
+  end  
+  program_file = File.join(program_file_dir,"programfile_of_#{ENV['USERNAME']}on#{ENV['PORT']}")
+  dir_rule = program_file_dir
+  STR0 = <<EOF
+peer p0=localhost:11110;
+collection ext persistent bootstrap@p0(atom1*);
+fact bootstrap@p0(1);
+fact bootstrap@p0(2);
+fact bootstrap@p0(3);
+fact bootstrap@p0(4);
+end
+EOF
+  File.open(program_file, 'w'){ |file| file.write STR0 }
+  @webdamlog = nil
+  # TODO find a good port for the wlengine
+  eval("@webdamlog = ClassWL#{ENV['USERNAME']}on#{ENV['PORT']}.new(peer_name,program_file,{:port => #{ENV['PORT']*2}, :dir_rule => dir_rule})")
+  @wlenginelogger = WLEnginelogger.new(STDOUT)
+  msg = "peer_name = #{peer_name} program_file = #{program_file} dir_rule = #{dir_rule}"
+  if @webdamlog.nil?
+    @wlenginelogger.fatal("creation of the webdamlog engine instance has failed: #{msg}")
+  else
+    @wlenginelogger.info("new instance of webdamlog engine created: #{msg}")
+  end
+  @webdamlog.run_bg
+
+  # XXX add action on shutdown for the wlengine such as erase program file if saved in db and clean rule dir if needed
+
+  # WLE:END
+
   WLPeer.send_acknowledgment(ENV['USERNAME'],ENV['MANAGER_PORT'],ENV['PORT'])
 
-  # Create a subclass of WL
-#  klass = Class.new(WLBud::WL)
-#  # TODO find a good name for the sub class, it should be uniq
-#  self.class.class_eval "ClassWL#{ENV['USERNAME']}on#{ENV['MANAGER_PORT']} = klass"
-#  peername="peername_#{ENV['USERNAME']}on#{ENV['MANAGER_PORT']}"
-#  peername="programfile_of_#{ENV['USERNAME']}on#{ENV['MANAGER_PORT']}"
-#  @webdamlog = eval("ClassWL#{ENV['USERNAME']}on#{ENV['MANAGER_PORT']}.new(:port => 12352)")
-#  @webdamlog.run_bg
-  
 end
--- lib/wl_logger.rb
+++ lib/wl_logger.rb
@@ -3,7 +3,7 @@ require 'logger'
 class WLLogger < Logger
   def debug message
     super "#{ENV['USERNAME']}:#{message.strip}"
-  end  
+  end
   def info message
     super "#{ENV['USERNAME']}:#{message.strip}"
   end
@@ -12,5 +12,20 @@ class WLLogger < Logger
   end
   def fatal message
     super "#{ENV['USERNAME']}:#{message.strip}"
+  end
+end
+
+class WLEnginelogger < WLLogger
+  def debug message
+    super "WLEngine:#{message.strip}"
+  end
+  def info message
+    super "#WLEngine::#{message.strip}"
+  end
+  def warn message
+    super "WLEngine::#{message.strip}"
+  end
+  def fatal message
+    super "WLEngine::#{message.strip}"
   end
 end
