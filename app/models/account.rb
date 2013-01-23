8261b8f1acc9bbbd73f5663973df099afdb45dba
jules testard <jules.testard@lsv.ens-cachan.fr>
2013-01-23 02:04:53 -0500

    About to start adding user and picture to the WLSchema (making them accessible to WLBud).

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,5 +2,5 @@ class Account < ActiveRecord::Base
   attr_accessible :ip, :port, :username
   attr_accessible :active
   validates :username, :format => { :with => /\A[a-zA-Z]+\z/,
-    :message => "Only letters allowed" }
+    :message => "Only letters allowed (between 5 and 20)", :length => {:minimum => 5, :maximum => 20} }
 end
---
+++ app/models/program.rb
@@ -0,0 +1,10 @@
+class Program < ActiveRecord::Base
+  
+  #This method returns a schema to be used in the WLSchema table
+  #to comply with the standards of that table. This should represent
+  #a hash of the attributes of this record.
+  #
+  def schema
+    
+  end
+end
