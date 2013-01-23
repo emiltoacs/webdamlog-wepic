83b6e976f061d58ec8f657defc3604522a6a6ed7
Emilien-Antoine <emilientoine@gmail.com>
2013-01-23 03:55:23 +0100

    add bud

--- config/environment.rb
+++ config/environment.rb
@@ -12,5 +12,16 @@ if WepimApp.is_manager?
   require 'lib/wl_launcher'
 else
   require 'lib/wl_peer'
+  require 'lib/webdamlog/wlbud'
   WLPeer.send_acknowledgment(ENV['USERNAME'],ENV['MANAGER_PORT'],ENV['PORT'])
+
+  # Create a subclass of WL
+#  klass = Class.new(WLBud::WL)
+#  # TODO find a good name for the sub class, it should be uniq
+#  self.class.class_eval "ClassWL#{ENV['USERNAME']}on#{ENV['MANAGER_PORT']} = klass"
+#  peername="peername_#{ENV['USERNAME']}on#{ENV['MANAGER_PORT']}"
+#  peername="programfile_of_#{ENV['USERNAME']}on#{ENV['MANAGER_PORT']}"
+#  @webdamlog = eval("ClassWL#{ENV['USERNAME']}on#{ENV['MANAGER_PORT']}.new(:port => 12352)")
+#  @webdamlog.run_bg
+  
 end
