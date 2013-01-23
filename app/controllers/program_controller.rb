b5833afded44deb53b2b3894ac0b82ba5326d3b4
jules testard <jules.testard@lsv.ens-cachan.fr>
2013-01-23 04:45:40 -0500

    The program tab is implemented. It is very late and I am going to bed. Emilien, if you have the time could you polish the CSS (the data is available its just a little ugly now).

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -1,4 +1,46 @@
+require 'yaml'
+require 'wl_logger'
 class ProgramController < ApplicationController
+  
   def index
+    @logger = WLLogger.new STDOUT
+    #Do not load program if already in main memory
+    @program = get_default_program if @program.nil?
+    
+    flash.now[:alert] = 'Default Program was not loaded properly.' unless @program
+    
+  end
+  
+  def get_default_program
+    #Get properties file
+    properties = YAML.load_file('config/properties.yml')
+    
+    #Initialize default program properties.
+    name = properties['default_program']['name']
+    author = properties['default_program']['author']
+    source = ""
+    unless properties['default_program']['source']=='./'
+      source = properties['default_program']['source']
+    end
+    
+    #Do not go further if the program has already been loaded from file.
+    program = Program.where(:name=>name)
+    return program if program
+    
+    filename = "#{name}"
+    data = ""
+    #Get the data attribute from the file.
+    begin
+    file = File.open(filename)
+    while line = file.gets
+      data += line+'\n'
+    end
+    rescue => error
+      @logger.info error.inspect
+      return nil
+    end
+    program =  Program.new(:name=>name,:author=>author,:source=>source,:data=>data)
+    return nil unless program.save
+    program
   end
 end
