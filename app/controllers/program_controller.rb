8f15c85cffbe9372c80705b9fcbde80c2c81f375
jules testard <jules.testard@lsv.ens-cachan.fr>
2013-01-29 22:25:45 -0500

    Switching branches, working on wrong branch

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -14,17 +14,21 @@ class ProgramController < ApplicationController
     #Get properties file
     properties = YAML.load_file('config/properties.yml')
     
+    #default values
+    name = "Unknown"
+    author = "Unknown"
+    source = ""    
+    
     #Initialize default program properties.
-    name = properties['default_program']['name']
-    author = properties['default_program']['author']
-    source = ""
-    unless properties['default_program']['source']=='./'
-      source = properties['default_program']['source']
-    end
+    name = properties['default_program']['name'] if properties['default_program']['name']
+    author = properties['default_program']['author'] if properties['default_program']['author']
+    source = properties['default_program']['source'] if properties['default_program']['source']
     
     #Do not go further if the program has already been loaded from file.
     program = Program.where(:name=>name)
     return program if program
+    
+    
     
     filename = "#{name}"
     data = ""
@@ -35,9 +39,17 @@ class ProgramController < ApplicationController
       data += line+'\n'
     end
     rescue => error
-      WLLogger.logger.info error.inspect
+      WLLogger.logger.warn error.inspect
       return nil
     end
+    
+    WLLogger.logger.info "Program Configuration"
+    WLLogger.logger.info name
+    WLLogger.logger.info author
+    WLLogger.logger.info source
+    WLLogger.logger.info data    
+    
+    #This is the table in the database that is storing the program
     program =  Program.new(:name=>name,:author=>author,:source=>source,:data=>data)
     return nil unless program.save
     program
