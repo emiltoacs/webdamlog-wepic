1117665ce6bfe393e57447eb1907c411179ebdbe
jules testard <jules.testard@lsv.ens-cachan.fr>
2013-01-30 00:40:19 -0500

    Change branch again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,26 +5,30 @@ class ApplicationController < ActionController::Base
   helper_method :is_admin
   helper_method :port
   
-  def initialize
-    @port = ENV['PORT']
-    super
-  end
-  
   private
   #TODO : need to find a clean way to define our admin list.
   #@@adminlist = Array[3,4,5]
   
   def current_user_session
-    return @current_user_session if defined?(@current_user_session)
+    return @current_user_session if @current_user_session
     @current_user_session = UserSession.find
+    WLLogger.logger.info "Current session not already defined. Looking for session : #{exists(@current_user_session,"empty")}"
+    @current_user_session
   end
   
   def current_user
-    return @current_user if defined?(@current_user)
+    return @current_user if @current_user
+    WLLogger.logger.info "Current User not already defined. Accessing session : #{exists(current_user_session,"no session")}"
     @current_user = current_user_session && current_user_session.record
   end
   
   def is_admin(user_id)
     return current_user.nil?#@@adminlist.include?(user_id)
+  end
+  
+  
+  #FIXME move to some utility module
+  def exists(a,b)
+    if a then a else b end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,35 +1,49 @@
+#The user class which defines the machine on which the user is.
+#
 class User < ActiveRecord::Base
-  db_name = "db/database_#{ENV['USERNAME']}.db"
-    @configuration = {:adapter => 'sqlite3', :database => db_name}
-  establish_connection @configuration
-  self.table_name = "users"
-  connection.create_table 'users', :force => true do |t|
-    t.string :username
-    t.string :email
-    t.string :crypted_password
-    t.string :password_salt
-    t.string :persistence_token
-    t.timestamps
-  end if !connection.table_exists?('users')
-  before_validation :default_values
+  
+  def self.setup
+    unless @setup_done
+      db_name = "db/database_#{ENV['USERNAME']}.db"
+      @configuration = {:adapter => 'sqlite3', :database => db_name}
+      establish_connection @configuration
+      self.table_name = "users"
+      connection.create_table 'users', :force => true do |t|
+        t.string :username
+        t.string :email
+        t.string :crypted_password
+        t.string :password_salt
+        t.string :persistence_token
+        t.timestamps
+      end if !connection.table_exists?('users')      
+      @setup_done = true
+    end
+  end
+  
   def default_values
     self.username ||= ENV['USERNAME']
   end  
-  acts_as_authentic do |c|
-    c.logged_in_timeout = 10.minutes # default is 10.minutes
-  end
+  
   def self.schema
     {'username' => 'string',
-     'email' => 'string',
-     'crypted_password' => 'string',
-     'password_salt' => 'string',
-     'persistence_token' => 'string'
+      'email' => 'string',
+      'crypted_password' => 'string',
+      'password_salt' => 'string',
+      'persistence_token' => 'string'
     }
   end
+  
   def self.open_connection
     establish_connection @configuration
   end
+  
   def self.remove_connection
     super
-  end  
+  end
+  
+  #Try to put class action at the end
+  setup
+  before_validation :default_values
+  acts_as_authentic do |c|
+  end
 end
