370dd57a38436d86b31895e8abe61824630a295e
Emilien-Antoine <emilientoine@gmail.com>
2013-02-13 11:20:39 +0100

    fix WLProgram that where changing one of its parameter, now I have a function sanitizer instead of proc

--- app/helpers/webdamlog_engine.rb
+++ app/helpers/webdamlog_engine.rb
@@ -1,4 +1,5 @@
 require 'wl_logger'
+require 'wl_tool'
 require 'webdamlog/wlbud'
 
 # There is the set of function used to manage the webdamlog engine from the
@@ -6,7 +7,8 @@ require 'webdamlog/wlbud'
 #
 module WebdamlogEngine
   
-  # TODO add action on shutdown for the wlengine such as erase program file if saved in db and clean rule dir if needed
+  # TODO add action on shutdown for the wlengine such as erase program file if
+  # saved in db and clean rule dir if needed
   #
   class WebdamlogEngine
 
@@ -26,28 +28,30 @@ EOF
       #Create a subclass of WL
       klass = Class.new(WLBud::WL)
       # TODO find a good name for the sub class, it should be uniq
-      username = UserConf.config[:connection]
-      wlport = ENV["PORT"]
-      self.class.class_eval "WLEngineOf#{username}On#{wlport} = klass"
+      username = UserConf.config[:name]
+      root_port = ENV["PORT"]
+      wlport = Network.find_ports('localhost', 1, root_port)
+      # Dynamic class ClassWLEngineOf#{username}On#{wlport} subclass WLBud
+      self.class.class_eval "ClassWLEngineOf#{username}On#{wlport} = klass"
       peer_name = "peername_#{username}on#{wlport}"
       # TODO find a good place to put the program file
-      program_file_dir = File.expand_path('../../tmp/rule_dir', __FILE__)
+      program_file_dir = File.expand_path('../../../tmp/rule_dir', __FILE__)
       unless (File::directory?(program_file_dir))
         Dir.mkdir(program_file_dir)
       end
       program_file = File.join(program_file_dir,"programfile_of_#{username}on#{wlport}")
       dir_rule = program_file_dir
       File.open(program_file, 'w'){ |file| file.write STR0 }
-      @webdamlog = nil
-      eval("@engine = ClassWL#{username}on#{wlport}.new(peer_name,program_file,{:port => #{wlport}, :dir_rule => dir_rule})")
+      @engine = nil
+      eval("@engine = ClassWLEngineOf#{username}On#{wlport}.new(username, program_file,{:port => #{wlport}, :dir_rule => dir_rule})")
       @wlenginelogger = WLLogger::WLEngineLogger.new(STDOUT)
       msg = "peer_name = #{peer_name} program_file = #{program_file} dir_rule = #{dir_rule}"
-      if @webdamlog.nil?
+      if @engine.nil?
         @wlenginelogger.fatal("creation of the webdamlog engine instance has failed:\n#{msg}")
       else
         @wlenginelogger.info("new instance of webdamlog engine created:\n#{msg}")
       end
-      #@webdamlog.run_bg
+      @engine.run_bg
     end
-  end  
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,15 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 WepimApp::Application.initialize!
 
-require 'wl_tool'
-require 'wl_logger'
+# There is the general custom configuration options for this app
+PeerConf.init
+db_name = "db/database_#{ENV['USERNAME']}.db"
+UserConf.init({
+    name: ENV['USERNAME'],
+    db_name: db_name,
+    connection: {:adapter => 'sqlite3', :database => db_name}
+  })
+
 #Once the application is initialized, if the application is a peer (and not a
 #manager), it signals to its manager that it is ready to receive requests).
 #
@@ -15,14 +22,6 @@ else
   require 'wl_peer'
   require 'webdamlog_engine'
   WLLogger.logger.info "Wepic peer of #{ENV['USERNAME']},#{ENV['PORT']} has finsihed initialization and is ready to send acknowedgement to manager"
+  wl = WebdamlogEngine::WebdamlogEngine.new
   WLPeer.send_acknowledgment(ENV['USERNAME'],ENV['MANAGER_PORT'],ENV['PORT'])
 end
-
-# There is the general custom configuration options for this app
-PeerConf.init
-db_name = "db/database_#{ENV['USERNAME']}.db"
-UserConf.init({
-    name: ENV['USERNAME'],
-    db_name: db_name,
-    connection: {:adapter => 'sqlite3', :database => db_name}
-  })
