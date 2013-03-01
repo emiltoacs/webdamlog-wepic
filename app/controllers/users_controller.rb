0877533f5b1a02f76abbcdc6a102159fec85dad1
Émilien <first.last@inria.fr>
2013-03-01 17:51:58 +0100

    bug in create table  in WL_database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,51 +47,57 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     flash[:notice] = params.inspect
-    respond_to do |format|
-      begin
-        WLDatabase.setup_database_server
-        if @user.save          
-          EngineHelper::WLHELPER.run
-          #When user is created, he is automatically logged in, which means
-          #we need to start his webdamlog session.
+    begin
+      
+      WLDatabase.setup_database_server
+      if @user.save
+        EngineHelper::WLHELPER.run
+        # When user is created, he is automatically logged in, which means we
+        # need to start his Webdamlog session.
+        respond_to do |format|          
           format.html { redirect_to(:wepic, :notice => "Registration successfull") }
-          format.xml { render :xml => @user, :status => :created, :location => @user }               
-        else
+          format.xml { render :xml => @user, :status => :created, :location => @user }
+        end
+      else
+        respond_to do |format| 
           format.html { render :action => "new" , :notice => params.inspect}
           format.xml { render :xml => @user.errors, :status => :unprocessable_entity }
-        end        
-      rescue Exception => error
+        end
+      end
+      
+    rescue Exception => error
+      respond_to do |format|
         format.html { render :action => "new", :alert => error.message }
         format.xml { render :xml => {setup: error.message}, :status => :unprocessable_entity}
       end
-    end
-  end
+    end # rescue Exception => error
+  end # create
 
-  # PUT /users/1
-  # PUT /users/1.json
-  def update
-    @user = User.find(params[:id])
+# PUT /users/1
+# PUT /users/1.json
+def update
+  @user = User.find(params[:id])
 
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, :notice => 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @user.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to :admin }
+  respond_to do |format|
+    if @user.update_attributes(params[:user])
+      format.html { redirect_to @user, :notice => 'User was successfully updated.' }
       format.json { head :no_content }
+    else
+      format.html { render :action => "edit" }
+      format.json { render :json => @user.errors, :status => :unprocessable_entity }
     end
   end
+end
+
+# DELETE /users/1
+# DELETE /users/1.json
+def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+
+  respond_to do |format|
+    format.html { redirect_to :admin }
+    format.json { head :no_content }
+  end
+end
 end
