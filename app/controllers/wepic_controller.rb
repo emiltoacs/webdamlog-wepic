4d07fec00d87e23ecc059667744fdeb750fa096c
jtestard <jtestard@jtestard-G73Sw.(none)>
2013-05-17 12:37:26 -0400

    making progress on pictures

--- app/controllers/wepic_controller.rb
+++ app/controllers/wepic_controller.rb
@@ -3,8 +3,28 @@ class WepicController < ApplicationController
   
   def index    
     @picture = Picture.new
-    @pictures = Picture.all
     @relation_classes = database(Conf.env['USERNAME']).relation_classes
-    @contacts = @relation_classes['Contact'].all
+    @pictures = @relation_classes['Picture'].all unless @relation_classes['Picture'].nil?    
+    @contacts = @relation_classes['Contact'].all unless @relation_classes['Contact'].nil?
+    unless @relation_classes['Location'].nil?
+      @locations = @relation_classes['Location'].all
+      @location = Hash.new
+      @pictures.each do |picture|
+        @location[picture.title] = find_location(picture)
+      end
+    end  
   end
+  
+  #This method assumes a relation called location exists with fields title, owner and place.
+  def find_location(picture)
+    unless @locations.nil?
+      @locations.each do |location|
+        if picture.title==location.title and picture.owner==location.owner
+          return location.place
+        end
+      end
+    else
+      nil
+    end
+  end  
 end
