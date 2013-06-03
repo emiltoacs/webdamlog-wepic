c8d8c5ddbda69e6e01b43771f80c129a370e11a6
Jules Testard <julestestard@MacBook-Pro-de-Jules.local>
2013-06-03 13:25:59 -0400

    comments well under way

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,17 @@
 class Comment < AbstractDatabase
-  attr_accessible :owner, :text, :title
+  attr_accessible :owner, :text, :title, :comment_owner
   def self.setup
     unless @setup_done      
       validates :owner, :presence => true
       validates :text, :presence => true
       validates :title, :presence => true
+      validates :comment_owner, :presence => true
       
       self.table_name = "comments"
       connection.create_table 'comments', :force => true do |t|
         t.string :title
         t.string :owner
+        t.string :comment_owner
         t.string :text
         t.timestamps
       end if !connection.table_exists?('comments')
@@ -25,7 +27,8 @@ class Comment < AbstractDatabase
   def self.schema
     {'title' => 'string',
      'owner' => 'string',
-     'text' => 'string'
+     'text' => 'string',
+     'comment_owner' => 'string'
      }
   end
   setup  
