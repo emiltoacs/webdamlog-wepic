22682a1afb3b49445f0817191c43b738cf22c41e
Émilien <first.last@inria.fr>
2013-06-07 19:58:22 +0200

    wdl changes

---
+++ lib/webdamlog/wlbud/version.rb
@@ -0,0 +1,10 @@
+# Used to track the version of the gem
+# Included in wlbud.gemspec
+# 
+# Specify the Bud version to use
+#
+module WLBud
+  VERSION = "0.0.1"
+  RUBY_VERSION = "1.9.3"
+  BUD_GEM_VERSION = '0.9.5'
+end
