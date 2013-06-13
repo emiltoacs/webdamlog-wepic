a557794ff5beff113631a1ceaf9f69872a015e60
Jules Testard <julestestard@MacBook-Pro-de-Jules.local>
2013-06-12 23:33:58 -0400

    in process awaiting merge

--- app/validators/wl_validator.rb
+++ app/validators/wl_validator.rb
@@ -3,8 +3,10 @@ require 'webdamlog/wlbud'
 class WlValidator < ActiveModel::EachValidator
   
   def validate_each(record, attribute, value)
-    response = EngineHelper::WLHELPER.parse(value)
+    response = EngineHelper::WLENGINE.parse(value)
+    WLLogger.logger.debug "WL parsing result : #{response.inspect}"
     #Check all elements from response and check if there is an error.
+    
     unless true#response.is_a?(WLBud::WLVocabulary)
       record.errors[attribute] << (options[:message] || response.message)
     end
