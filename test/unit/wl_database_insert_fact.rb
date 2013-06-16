078b585164830ee3c880461c2cdf2d4c84219eb4
Émilien <first.last@inria.fr>
2013-06-17 01:30:35 +0200

    minor

--- test/unit/wl_database_insert_fact.rb
+++ test/unit/wl_database_insert_fact.rb
@@ -21,7 +21,7 @@ class WLDatabaseInsertViaActiveRecord < Test::Unit::TestCase
     helper.run_engine
     engine = EngineHelper::WLENGINE
     engine.load_bootstrap_fact
-    db.save_facts_for_meta_data    
+    db.save_facts_for_meta_data
 
     db.relation_classes['Contact'].new(:username=>'name',:peerlocation=>'peerlocation',:online=>false,:email=>'email',:facebook=>'facebook').save
 
