75c96668cdf05645e25b1d368e26af27c92d726f
Emilien-Antoine <emilientoine@gmail.com>
2013-06-17 01:10:46 +0200

    improve test

--- test/unit/models_test.rb
+++ test/unit/models_test.rb
@@ -1,35 +1,27 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
-$:.unshift File.join(File.dirname(__FILE__),'..','lib')
-
+# setup environment before loading wltool.rb in wl_setup.rb
+ENV["RAILS_ENV"] = "test"
+ENV["USERNAME"] = "modelstesttest"
+ENV["PORT"] = "10000"
+ENV["MANAGER_PORT"] = nil
+require 'wl_tool'
+Conf.db['database']="wp_modelstesttest"
 require 'test/unit'
-require 'test_helper'
+require './lib/wl_setup'
 
 class ModelsTest < Test::Unit::TestCase
   
-  def setup
-    @dbid = (0...8).map{('a'..'z').to_a[rand(26)]}.join
-    ENV['USERNAME'] = @dbid
-
-    # reload the models to allow builtins tables to be created
-    @id = @dbid
-    @db_name = 'wp_test'
-    @configuration = {:adapter => 'postgresql', :database => @db_name}
-    puts "Configuration : id=#{@id.inspect}, db_name=#{@db_name.inspect}, configuration=#{@configuration.inspect}"
-    #PostgresHelper::create_user_db #@configuration
-    #@database = WLDatabase.establish_orm_db_connection(@id,@db_name,@configuration)
-  end
-  
-  def teardown
-    #@database.destroy
-  end
-  
-  def test_simple
-    #Test environment
-  end
-  
   def test_rating
+    WLSetup.reset_peer_databases Conf.db['database'], Conf.db['username'], Conf.db['adapter']
+    require 'test_helper'
+    db = WLDatabase.setup_database_server
+    assert_not_nil db
+    helper = EngineHelper::WLHELPER
+    helper.run_engine
+    engine = EngineHelper::WLENGINE
+    engine.load_bootstrap_fact
+    db.save_facts_for_meta_data
+
+    
     rating = Rating.new(:rating => 3, :owner=>'jules', :_id => 12345)
     rating.save
     assert_equal(3,rating.rating)
@@ -37,14 +29,14 @@ class ModelsTest < Test::Unit::TestCase
   
   def test_new_picture_remote
     #TODO: Write test #:date => DateTime.now,
-    picture = Picture.new(:owner=>"Emilien",:title=>"nemo") #:remote_image_url=>"http://1.bp.blogspot.com/-Gv648iUY5p0/UD8rqW3deSI/AAAAAAAAACA/MrG4KxFyM5A/s400/Fish.jpeg"
-    picture.save
-    assert_equal(picture.image_file_name,"Fish.jpeg")
-    assert_equal(picture.image_file_size,32824)
-    assert_equal(picture.image_content_type,"image/jpeg")
-    assert_equal(picture.owner,"Emilien")
-    assert_equal(picture.title,"nemo")
-    picture.destroy
+#    picture = Picture.new(:owner=>"Emilien",:title=>"nemo") #:remote_image_url=>"http://1.bp.blogspot.com/-Gv648iUY5p0/UD8rqW3deSI/AAAAAAAAACA/MrG4KxFyM5A/s400/Fish.jpeg"
+#    picture.save
+#    assert_equal(picture.image_file_name,"Fish.jpeg")
+#    assert_equal(picture.image_file_size,32824)
+#    assert_equal(picture.image_content_type,"image/jpeg")
+#    assert_equal(picture.owner,"Emilien")
+#    assert_equal(picture.title,"nemo")
+#    picture.destroy
   end
 #   
   # # def test_new_picture_local
