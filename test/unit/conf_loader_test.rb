324c9e3a6df8fbf9579801b1338e2b66187c9a25
Émilien <first.last@inria.fr>
2013-06-17 16:01:38 +0200

    split conf files to allow mulitple profile

--- test/unit/conf_loader_test.rb
+++ test/unit/conf_loader_test.rb
@@ -9,29 +9,21 @@ class ConfTest < Test::Unit::TestCase
     assert rails_env.test?
     assert_equal "test", rails_env
     Conf.init(:rails_env=>rails_env)
-    assert_not_nil(Conf.peer['manager']['manager_port'])
-    assert_equal 4100, Conf.peer['manager']['manager_port']
+    assert_not_nil(Conf.peer['manager'])
     assert_not_nil(Conf.peer['manager']['default_spawn_port'])
-    assert_equal 30000, Conf.peer['manager']['default_spawn_port']
-    
-    assert_equal "http", Conf.peer['peer']['protocol']
+
+    assert_not_nil Conf.peer['peer']['username']
+    assert_not_nil Conf.peer['peer']['protocol']
     assert_equal "localhost", Conf.peer['peer']['ip']
-    assert_equal 10000, Conf.peer['peer']['web_port']
-    assert_equal "prog1.wl", Conf.peer['peer']['program']['name']
-    assert_equal "app/assets/wlprogram/prog1.wl", Conf.peer['peer']['program']['source']
+    assert_not_nil Conf.peer['peer']['web_port']
+    assert_not_nil Conf.peer['peer']['program']['file_path']
 
     assert_equal "postgresql", Conf.db['adapter']
-    assert_equal "manager", Conf.db['username']
-    assert_equal "password", Conf.db['password']
+    assert_equal "wepic", Conf.db['username']
+    assert Conf.db['password'].nil?
     assert_equal "unicode", Conf.db['encoding']
-    assert_equal 5, Conf.db['pool']
-    assert_equal "db_test", Conf.db['database']
-    
-
-    # These variable are set by wl_setup so see wl_setup test for more checking
-#    assert_equal "", Conf.env['USERNAME']
-#    assert_equal "", Conf.env['PORT']
-#    assert_equal "", Conf.env['MANAGER_PORT']
+    assert_not_nil Conf.db['pool']
+    assert_equal "wp_test", Conf.db['database']
   end
   
 end
