e2ea7b2e69fa77e73376670fd8a91e4ad6ebbc19
Jules Testard <julestestard@MacBook-Pro-de-Jules.local>
2013-06-17 15:02:54 -0400

    manager port fixed

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -5,7 +5,8 @@ module ContentHelper
     if defined?(Conf)
       WLLogger.logger.debug "Query Samples for user : #{Conf.env['USERNAME']}"
       sample_content_file_name = Conf.peer['peer']['program']['query_sample']
-      if (File.exists?(sample_content_file_name))
+      require 'debugger';debugger
+      if (File.exists?("#{Rails.root}/#{sample_content_file_name}"))
         content = YAML.load(File.open(sample_content_file_name))
         #WLLogger.logger.debug 'Reseting described rules...' if DescribedRule.delete_all
         content['described_rules'].values.each do |idrule|
