3815cebc9c4859f04742bde88636c9779494aed6
Jules Testard <julestestard@MacBook-Pro-de-Jules.local>
2013-06-24 18:25:59 -0400

    url validation correct

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -2,19 +2,19 @@ require 'open-uri'
 
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || "must be a valid URL") unless url_valid?(value)
+    record.errors[attribute] << (options[:message] || "must be a valid URL") unless url_valid?(record,value)
   end
 
   # a URL may be technically well-formed but may 
   # not actually be valid, so this checks for both.
-  def url_valid?(url)
-    if url.nil? #image_url not specified
+  def url_valid?(tuple,url)
+    if url.nil? and tuple.is_a?(Picture) and !tuple.image.nil? #image_url not specified for Picture tuple
       true
     else
       url = URI.parse(url) rescue false
       url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
-      if url!='localhost' #url is from the outside
-        true
+      if url.host!='localhost' #url is from the outside
+        Net::HTTP.get_response(url).is_a?(Net::HTTPOK)
       else #url must be intra webdamlog don't check
         true
       end
